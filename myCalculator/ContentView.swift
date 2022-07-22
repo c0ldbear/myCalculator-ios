6ee47674d46b883831595bd40171af50e596d43b
teddy <teddy.juhlin-henricson@bontouch.com>
2022-07-22 09:57:12 +0200

    Add test @States, ZStack and some text and buttons

--- myCalculator/ContentView.swift
+++ myCalculator/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text = "Hello"
+    @State var favorite = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            
+            VStack {
+                Text(text)
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                
+                HStack {
+                    Button("1", action: {
+                        text = "1"
+                    })
+                    .padding()
+                    .background(Color.accentColor)
+                    .foregroundColor(.white)
+                    
+                    Button("2", action: {
+                        text = "2"
+                    })
+                    .padding()
+                    .background(Color.accentColor)
+                    .foregroundColor(.white)
+                    
+                    Button("3", action: {
+                        text = "3"
+                    })
+                    .padding()
+                    .background(Color.accentColor)
+                    .foregroundColor(.white)
+                }
+                HStack {
+                    Button(action: {
+                        favorite.toggle()
+                    }) {
+                        Image(systemName: favorite ? "star.fill" : "star")
+                            
+                    }
+                    Button(action: {
+                        text = "5"
+                    }) {
+                        Text("5")
+                    }
+                }
+                .buttonStyle(CalcButton())
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CalcButton: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(.cyan)
+            .foregroundColor(.white)
     }
 }
