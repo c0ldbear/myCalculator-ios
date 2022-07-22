6596a09f8486785e1fd1666d21b5987f088e2c6e
teddy <teddy.juhlin-henricson@bontouch.com>
2022-07-22 15:06:01 +0200

    Add a view model

---
+++ myCalculator/CalculatorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CalculatorViewModel.swift
+//  myCalculator
+//
+//  Created by teddy juhlin-henricson on 2022-07-22.
+//
+
+import Foundation
+
+final class CalculatorViewModel: ObservableObject {
+    
+}
