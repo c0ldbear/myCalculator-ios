cb043e0e333e7e139ca5958b6ace708d8ad0832a
teddy <teddy.juhlin-henricson@bontouch.com>
2022-07-22 09:09:37 +0200

    Initial Commit

---
+++ myCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myCalculator
+//
+//  Created by teddy juhlin-henricson on 2022-07-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myCalculator/myCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  myCalculatorApp.swift
+//  myCalculator
+//
+//  Created by teddy juhlin-henricson on 2022-07-22.
+//
+
+import SwiftUI
+
+@main
+struct myCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
