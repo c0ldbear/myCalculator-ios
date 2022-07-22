2a60d98a6c37cb20c656eb864254aeeaecbbfa77
teddy <teddy.juhlin-henricson@bontouch.com>
2022-07-22 10:33:12 +0200

    Add enum for the text string used by the buttons

---
+++ myCalculator/CalcButtonText.swift
@@ -0,0 +1,18 @@
+//
+//  CalcButtonText.swift
+//  myCalculator
+//
+//  Created by teddy juhlin-henricson on 2022-07-22.
+//
+
+import Foundation
+
+enum CalcButtonText: String {
+    case start = "Welcome"
+    case star = "Star!"
+    case noStar = "!Star"
+    case empty = ""
+    case one = "1"
+    case two = "2"
+    case three = "3"
+}
