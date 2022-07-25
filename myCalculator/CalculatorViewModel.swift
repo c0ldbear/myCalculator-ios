6da6c92cc8e2f09189d2f83477b82fbda963a8cd
teddy <teddy.juhlin-henricson@bontouch.com>
2022-07-25 10:31:38 +0200

    Add a CalculatorViewModel

--- myCalculator/CalculatorViewModel.swift
+++ myCalculator/CalculatorViewModel.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 final class CalculatorViewModel: ObservableObject {
+    @Published private(set) var calculatorModel = CalculatorModel()
     
+    func setInput1(_ input: String) {
+        calculatorModel.setInput1(Double(input))
+    }
+    
+    func setInput2(_ input: String) {
+        calculatorModel.setInput2(Double(input))
+    }
+    
+    func setOperation(_ input: String) {
+        calculatorModel.setOperator(CalculatorModel.Operation(input)) // Change to have method 'setOperator' to have string as input?
+    }
+    
+    func calculateResult() -> String {
+        return "\(calculatorModel.getResult())"
+    }
 }
