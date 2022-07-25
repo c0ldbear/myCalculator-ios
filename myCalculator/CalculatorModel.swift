440ac1100825da5ef0934d93e3043f4e56924dd6
teddy <teddy.juhlin-henricson@bontouch.com>
2022-07-25 10:32:25 +0200

    Change the data model from strings to doubles

--- myCalculator/CalculatorModel.swift
+++ myCalculator/CalculatorModel.swift
@@ -8,45 +8,67 @@
 import Foundation
 
 struct CalculatorModel {
-   enum Operation {
+    enum Operation {
         case addition
         case substraction
         case multiplication
         case division
+        case none
+        
+        init(_ operation: String) {
+            switch operation.lowercased() {
+            case "addition":
+                self = .addition
+            case "substraction":
+                self = .substraction
+            case "multiplication":
+                self = .multiplication
+            case "division":
+                self = .division
+            default:
+                self = .none
+                
+            }
+        }
     }
     
-    private var input1: String?
-    private var input2: String?
-    private var result: String?
+    private var input1: Double?
+    private var input2: Double?
+    private var result: Double?
     private var currentOperator: Operation? 
     
    
-    mutating func setInput1(_ input: String) {
+    mutating func setInput1(_ input: Double?) {
         input1 = input
     }
     
-    mutating func setInput2(_ input: String) {
+    mutating func setInput2(_ input: Double?) {
         input2 = input
     }
     
-    mutating func setOperator(_ selectedOperator: Operation) {
+    mutating func setOperator(_ selectedOperator: Operation?) {
         currentOperator = selectedOperator
     }
     
-    mutating func calculateResult(input1: String, input2: String) {
-        let double1 = Double(input1) ?? 0
-        let double2 = Double(input2) ?? 0
+    mutating func getResult() -> Double {
+        calculateResult()
+        return result ?? 0
+    }
+    
+    mutating func calculateResult() {
+        let double1 = input1 ?? 0
+        let double2 = input2 ?? 0
         
         switch currentOperator {
         case .addition:
-            result = String(double1 + double2)
+            result = double1 + double2
         case .substraction:
-            result = String(double1 - double2)
+            result = double1 - double2
         case .multiplication:
-            result = String(double1 * double2)
+            result = double1 * double2
         case .division:
-            result = double2 == 0 ? "0" : String(double1 / double2)
-        case .none:
+            result = double2 == 0 ? 0 : double1 / double2
+        default:
             return
         }
     }
